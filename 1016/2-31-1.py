50e58d5641a11b00b805cc3f389db53f69abd6b3
andy <cool85058@gmail.com>
2025-10-16 14:32:03 +0800

    10/16 練習題目

--- 1016/2-31-1.py
+++ 1016/2-31-1.py
@@ -3,6 +3,10 @@ binx = bin(x)[2:]  # 去掉 '0b' 前綴
 octx = oct(x)[2:]
 hexx = hex(x)[2:]
 
+# 中文字無法很完美的對齊
+print(f'{"二進位":>10s}' f'{"八進位":>10s}' f'{"十六進位":>10s}')
+print(f"{binx:>10s}" f"{octx:>10s}" f"{hexx:>10s}")
 
-print(f'{"二進位":6s}' f'{"八進位":6s}' f'{"十六進位":6s}')
-print(f"{binx:>6s}" f"{octx:>9s}" f"{hexx:>9s}")
+# 使用英文字對齊
+print(f'{"bin":>10s}' f'{"oct":>10s}' f'{"hex":>10s}')
+print(f"{binx:>10s}" f"{octx:>10s}" f"{hexx:>10s}")
