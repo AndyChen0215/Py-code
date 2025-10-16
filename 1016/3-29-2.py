26218e6991761820445618d78c6b398a4c66540c
Andy <cool85058@gmail.com>
2025-10-16 16:02:46 +0800

    10/16 練習題目

--- 1016/3-29-2.py
+++ 1016/3-29-2.py
@@ -7,3 +7,13 @@ area = ((math.sqrt(3)) / 4) * math.pow(s, 2)
 
 print("高 = " f"{h}")
 print("面積 = " f"{area}")
+
+
+# 如果不使用 import math 的寫法
+print("\n不使用math套件")
+
+h = ((3**0.5)/2) * s
+area = ((3**0.5)/4) * (s*s)
+
+print("高 = " f'{h}')
+print("面積 = " f'{area}')
