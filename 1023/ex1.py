9ea5c45ec66e09e3d0f5a8cb96dba5ea2a42cab2
andy <cool85058@gmail.com>
2025-10-23 14:38:39 +0800

    10/23 練習題目

--- 1023/ex1.py
+++ 1023/ex1.py
@@ -5,3 +5,13 @@ c = eval(input())
 ans = (4/(3*(a+34)))-9*(a+b*c)+((3+3*(2+a))/(a+3*b))
 
 print(ans)
+
+# 拆開來的解法
+
+term1 = 4 / (3 * (a + 34))
+term2 = 9 * (a + b * c)
+term3 = (3 + 3 * (2 + a)) / (a + 3 * b)
+
+ans = term1 - term2 + term3
+
+print(ans)
